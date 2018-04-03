97996348a84f3e2ee9eca384cf3d2ac1dca29c6b
桥公子 <373216956@qq.com>
2018-04-03 21:35:20 +0800

    修改打开新页面被拦截

--- src/main/webapp/vent/common/js/service/venttp.service.js
+++ src/main/webapp/vent/common/js/service/venttp.service.js
@@ -11,16 +11,19 @@ define('venttp.module', function (require, exports) {
             return service;
         });
         md.factory('httpUrl', function () {
-            //本地idea启动
-            var appUrl = 'http://localhost:80/';
-            //部署tomcat启动
-            // var appUrl = 'http://192.168.131.24/';
+            //本地idea启动tomcat
+            var appUrl = 'http://localhost/';
+            //tomcat启动,为本机IP地址,需在Connector标签加入adress="0.0.0.0"
+            //以及在Host标签中加上<Context path="" docBase="venttp" debug="0" reloadable="true"/>
+            // var appUrl = 'http://192.168.1.4/';
             var service = {};
             service.getPostUrl = function (url) {
                 return appUrl + url;
             };
             service.openPage = function (url) {
-                window.open(service.getPostUrl(url));
+                $('body').append($('<a href="'+service.getPostUrl(url)+'" target="_blank" id="openWin"></a>'))
+                document.getElementById("openWin").click();//点击事件
+                $('#openWin').remove();
             };
             return service;
         })
